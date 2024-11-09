9f88a7e848bafc247eb222e425766d3f262ac0fe
georgebrianb <george_brianb@yahoo.com>
2024-11-09 18:04:38 +0100

    modified hook

--- extensions/hooks/binary-answer.js
+++ extensions/hooks/binary-answer.js
@@ -1,6 +1,6 @@
 module.exports = function registerHook({ filter }) {
   filter('items.create', 'quiz_questions', async (input) => {
-    const { answer_type, selected_answers, choices } = input;
+    const { question_type, selected_answers, choices } = input;
 
     if (selected_answers && choices) {
       // Initialize binary string with 0s
@@ -8,7 +8,8 @@ module.exports = function registerHook({ filter }) {
 
       // Set "1" in positions corresponding to selected answers
       choices.forEach((choice, index) => {
-        if (selected_answers.includes(choice)) {
+        const choiceKey = choice.toUpperCase();  // Assuming choices are A, B, C, D, E
+        if (selected_answers[choiceKey]) {
           binaryAnswer[index] = '1';
         }
       });
@@ -17,12 +18,12 @@ module.exports = function registerHook({ filter }) {
       const binaryString = binaryAnswer.join('');
 
       // Validation: Single Answer type should have exactly one "1"
-      if (answer_type === 'Single Answer' && binaryString.split('1').length - 1 !== 1) {
+      if (question_type === 'single_answer' && binaryString.split('1').length - 1 !== 1) {
         throw new Error('Single Answer questions must have exactly one correct answer.');
       }
 
       // Validation: Multiple Answers type should have between 2 and 4 "1"s
-      if (answer_type === 'Multiple Answers') {
+      if (question_type === 'multiple_answers') {
         const countOnes = binaryString.split('1').length - 1;
         if (countOnes < 2 || countOnes > 4) {
           throw new Error('Multiple Answer questions must have between 2 and 4 correct answers.');
